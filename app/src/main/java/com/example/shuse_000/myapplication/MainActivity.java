945bc1dda1a6a321a7afe78f4a52d1b4878fad1e
sshrest9 <shusen@live.ca>
2017-11-18 10:53:54 -0500

    Did camera code

--- app/src/main/java/com/example/shuse_000/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shuse_000/myapplication/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.shuse_000.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends Activity {
+    Button btn;
+    ImageView imageView;
+    static final int CAM_REQUEST=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= findViewById(R.id.picBtn);
+        imageView= findViewById(R.id.imgCap);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file= getFile();
+                camera.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(file));
+                startActivityForResult(camera,CAM_REQUEST);
+            }
+        });
+    }
+
+    private File getFile() {
+
+        File folder = new File("sdcard/camera_app");
+
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
+        File img_file = new File(folder, "cam_img.jpg");
+        return img_file;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String path="sdcard/camera_app/cam_img.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
+        if (requestCode == CAM_REQUEST && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
+
